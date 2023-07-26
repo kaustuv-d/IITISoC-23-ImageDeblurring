7a1543d093003221c2e37bc519a895d661e29a54
Ruthvik283 <118649353+Ruthvik283@users.noreply.github.com>
2023-07-26 22:41:09 +0530

    Update script.js

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -15,7 +15,8 @@ function deblur1() {
   var ruth = document.getElementById('abc');
   // ruth.innerHTML = '<div id="preview"><input type="file" name="upload_file" class="form-control" placeholder="Enter Name" id="upload_file" onchange="getImagePreview(event)"></div>';
   //ruth.innerHTML = '<form method="post" action="/" enctype="multipart/form-data" ><dl><p><input type="file" name="file" class="form-control" autocomplete="off" required></p></dl><p><input type="submit" value="Submit" class="btn btn-info"></p></form>'
-  ruth.innerHTML = '<div style="text-align:center;" id="posh"></div><br><form method="post" action="/" enctype="multipart/form-data" ><dl><div><p><input type="file" name="file" class="form-control" autocomplete="off" required onchange="getImagePreview(event)"></p><div></dl><p><input type="submit" value="Submit" class="btn btn-info" ></p></form>'
+  //ruth.innerHTML = '<div style="text-align:center;" id="posh"></div><br><form method="post" action="/" enctype="multipart/form-data" ><dl><div><p><input type="file" name="file" class="form-control" autocomplete="off" required onchange="getImagePreview(event)"></p><div></dl><p><input type="submit" value="Submit" class="btn btn-info" ></p></form>'
+  ruth.innerHTML = '<div style="text-align:center;" id="posh"></div><br><form method="post" action="/" enctype="multipart/form-data" ><dl><div><p><input type="file" name="file" class="form-control" autocomplete="off" required onchange="getImagePreview(event)"></p></div></dl><br><input type="submit" value="Submit" hidden/><button class="bbb""><div style = "z-index: 100; background-color: black; height: 40px; width: 120px; border-radius: 8px;text-align:center;"><div style = "margin-top: 5px;">Submit</div></div></button><br></form>';
 } 
 
 function bgm()
