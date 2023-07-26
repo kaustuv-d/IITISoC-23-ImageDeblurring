63670555bf3fef2ecae180c0e0e8d63c38b09586
Kaustuv Devmishra <117767351+kaustuv-d@users.noreply.github.com>
2023-07-26 22:29:17 +0530

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -1,27 +1,9 @@
-# from flask import Flask,render_template
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# def welcome():
-#     return render_template('index.html')
-#     # return "fo"
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-# 1. creating Flask application
-# 2. creating .pkl file of our Model
-# 3. adding respective functionalities of buttons/ creating api's 
 
 from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
 import os
 from werkzeug.utils import secure_filename
-import pickle
 from PIL import Image
-# import torchvision
 import torchvision.transforms as transforms
 import layers
 import torch
